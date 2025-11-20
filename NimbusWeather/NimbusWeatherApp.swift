4ac138fe91d87202b0efa31723d393f7d70a383e
Baris Gorgun <BARISGO1@intertech.com.tr>
2025-11-20 11:36:06 +0300

    Initial Commit

---
+++ NimbusWeather/NimbusWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusWeatherApp.swift
+//  NimbusWeather
+//
+//  Created by Gorgun, Baris on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NimbusWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
